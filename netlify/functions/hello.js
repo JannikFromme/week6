b410f1accebbbaf5935ecbd64202a33fb0879083
Jannik Fromme <jannik.fromme@yahoo.de>
2021-05-04 11:48:00 -0500

    commit

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -4,6 +4,8 @@ exports.handler = async function(event) {
   // a lambda function returns a status code and a string of data
   return {
     statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-    body: `Hello from the back-end!` // a string of data
+    body: `Hello from the back-end yeah!` // a string of data
   }
 }
+
+// API can be found under: http://localhost:8888/.netlify/functions/hello (netlify logic)
--- netlify/functions/todos.js
+++ netlify/functions/todos.js
@@ -2,16 +2,22 @@
 exports.handler = async function(event) {
 
   // an Array of to-dos
+  let todos = [`go`,`went`,`gone`]
 
   // write the Array of to-dos to the *back-end* console
+  console.log(todos)
 
   // turn the Array into a String
+  let dataAsString = JSON.stringify(todos)
 
   // write the "stringified" Array to the *back-end* console
+  console.log(dataAsString)
 
   // a lambda function returns a status code and a string of data
   return {
     statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-    body: `Hello from the back-end!` // a string of data
+    body: dataAsString // a string of data
   }
 }
+
+// http://localhost:8888/.netlify/functions/todos
